5cff8cd1aa6cc519c46c9edecdd4a4178bddeb1f
Michelli Venerando <mvenerando@crm.local>
2026-03-26 18:46:59 -0300

    SEPARACAO POR PERFIL

--- index.js
+++ index.js
@@ -177,7 +177,7 @@ app.get("/perfil", async (req, res) => {
     // 🔥 buscar perfil na tabela usuarios
     const perfilResponse = await fetch(
       process.env.SUPABASE_URL +
-        "/rest/v1/usuarios?id=eq." + userData.id,
+        "/rest/v1/usuarios?email=eq." + userData.email
       {
         headers: {
           "apikey": process.env.SUPABASE_KEY,
